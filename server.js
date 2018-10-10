0aa2e0e98c43c32c61bf74cab31eb5b8cf6ccc50
tdesosa <tdesosa@gmail.com>
2018-10-10 12:01:57 -0600

    Delete From Created

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+// Setup Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
 // Require The Controller
 const soccerTeamsController = require('./controllers/soccerTeamsController')
 
